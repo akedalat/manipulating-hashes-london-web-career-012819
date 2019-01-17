cd6be6e611dfa51bd812f879075d547f62ff79bf
Kian Edalat <akedalat@gmail.com>
2019-01-17 16:51:04 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,14 +14,14 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"].each do |k, v| 
-    if k == :favorite_icecream_flavors 
+  contacts["Freddy Mercury"].each do |k, v|
+    if k == :favorite_icecream_flavors
       k.delete_if do |array|
         array == "strawberry"
     end
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+  end
 end
 
+  #remember to return your newly altered contacts hash!
+return contacts
+end
